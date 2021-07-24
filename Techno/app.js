240064f0c0232e633c607d83eb08c8103dfb6d63
Savio Nascimento <savionascimentodev@gmail.com>
2021-07-24 19:04:05 -0400

    ✨ Completed classa 8

---
+++ Techno/app.js
@@ -0,0 +1,43 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    products: [],
+    product: false,
+  },
+  filters: {
+    numberPrice(value) {
+      return value.toLocaleString("pt-br", { style: "currency", currency: "BRL" })
+    },
+  },
+  methods: {
+    fetchProducts() {
+      fetch("./api/products.json")
+        .then(r => r.json())
+        .then(r => {
+          this.products = r
+        })
+    },
+    fetchProduct(id) {
+      fetch(`./api/products/${id}/data.json`)
+        .then(r => r.json())
+        .then(r => {
+          this.product = r
+        })
+    },
+    openModal(id) {
+      this.fetchProduct(id)
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    closeModal({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.product = false
+      }
+    }
+  },
+  created() {
+    this.fetchProducts()
+  }
+});
